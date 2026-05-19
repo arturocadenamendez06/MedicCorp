715a9f92fda86958a2d6f7e50834b6800ff1f5f9
VickySS2020 <A22216352@alumnos.uady.mx>
2026-05-19 15:52:20 -0600

    Servidor de consultorio agregado
    Incluye todas las dependencias que vamos a usar para el proyecto y el archivo "server.js" para ejecutar el servidor de la capa de negocio.

---
+++ MedicCorp/server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000; //Puerto del backend, modificar si es necesario
+//const dotenv = require('dotenv');
+
+//dotenv.config();
+//const dbService = require('./dbService');
+
+//app.use(cors());
+//app.use(express.json());
+
+app.get('/', (req, res) => {
+    console.log('Received a request');
+    res.send('Hello World');
+})
+
+app.listen(port, () => {
+    console.log('The server is running...');
+})
